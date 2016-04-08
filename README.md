c898da940e9c167631816ec635f6e6aa0c7b0f13
LambdaXymox <lambda.xymox@gmail.com>
2016-04-08 02:14:51 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Bookworm
+
+**Bookworm** is a set of python scripts for automating parts of the workflow
+for assembling PDF files.
+
+These are some python scripts for automating PDF file manipulation workflow. They
+are meant for normalizing the dimensions of scanned pages and ensuring they are all
+the same resolution. These help automate the process of preparing the scanned pages of
+a book for packing into a PDF or DJVU and running optical character recognition.
+
+Usage
+=====
+
+Fork [fork]https://github.com/stallmanifold/bookworm and run
+
+    python bookworm.py /path/to/book/pages
+    
+or
+    
+    python bookworm.py /path/to/pdf/file  
+    
+in the main directory.
---
+++ bookworm.py
@@ -0,0 +1,81 @@
+import subprocess
+import os.path
+
+from Enum import Enum
+
+
+class ResolutionUnits(Enum):
+    PixelsPerInch       = 1
+    PixelsPerCentimeter = 2
+
+    def __repr__(self):
+        if self == PixelsPerInch:
+            return "PixelsPerInch"
+        else:
+            return "PixelsPerCentimeter"
+
+    def as_terminal_arg(self):
+        return repr(self)
+
+
+class Resolution:
+    def __init__(resolution, units):
+        self.resolution = resolution
+        self.units = units
+
+
+def temp_filename(file_name):
+    return "{}.tmp".format(file_name)
+
+
+def cleanup(file_name):
+    os.remove(file_name)
+
+
+"""
+The main pdf operations. The primary operation are:
+1. Unpack a PDF.
+2. Pack a directory of images into a PDF.
+3. Change image resolution.
+4. Rescale image.
+5. Expand image with fill.
+"""
+
+def unpack_pdf(file_name):
+    pass
+
+def pack_pdf(directory):
+    pass
+
+def change_resolution(resolution, image_file):
+    new_image_file = temp_filename(image_file)
+    density = "-density {}".format(resolution.resolution)
+    units = "-units {}".format(resolution.units)
+
+    try:
+        subprocess.run(["convert", density, units, new_image_file])
+    except subprocess.CalledProcessError as e:
+        cleanup(new_image_file)
+        raise e
+    
+
+        
+def rescale(resolution, image_file):
+    gravity = "-gravity center"
+
+
+def expand_file_with_fill(dimensions, image_file):
+    pass
+
+
+def usage():
+    return   'python bookworm.py [-options] /path/to/image/files/\n'
+           + 'or\n'
+           + 'python bookworm.py [-options] /path/to/pdf/file\n'
+
+def main():
+    usage()
+
+
+if __name__ == 'main':
+    main()
