be949f4959e00d683cd85cef80de746e36f65a61
LambdaXymox <lambda.xymox@gmail.com>
2017-11-05 09:35:06 +0000

    Add test for Resolution class.

--- tests/test_resolution.py
+++ tests/test_resolution.py
@@ -1,8 +1,17 @@
 import unittest
-from bookworm.resolution import Resolution
+from bookworm.resolution import Resolution, ResolutionUnits
 
 
 class TestResolution(unittest.TestCase):
+
+    def test_make_resolution_should_accept_correct_input(self):
+        resolution_val = 600
+        resolution_units = 'PixelsPerInch'
+        resolution = Resolution.make_resolution(resolution_val, resolution_units)
+
+        self.assertIsInstance(resolution, Resolution)
+        self.assertEqual(resolution.units, ResolutionUnits.PixelsPerInch)
+
 
     def test_make_resolution_should_only_accept_certain_units(self):
         resolution_val = 600
@@ -16,6 +25,7 @@ class TestResolution(unittest.TestCase):
         resolution_units = 'PixelsPerInch'
 
         self.assertRaises(ValueError, Resolution.make_resolution, resolution_val, resolution_units)
+
 
     def test_make_resolution_should_reject_non_integer_values(self):
         resolution_val = 600.1
