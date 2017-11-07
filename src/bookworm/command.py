d757124b990c90a1819967f57b80a4b51b087873
LambdaXymox <lambda.xymox@gmail.com>
2017-11-07 11:04:21 +0000

    Remove command execution methods from Command interface.

--- src/bookworm/command.py
+++ src/bookworm/command.py
@@ -15,24 +15,25 @@ class Command:
     def as_python_subprocess(self):
         return NotImplemented
 
-    def setup(self):
-        return NotImplemented
-
-    def run(self):
-        """
-        Execute a command in the shell.
-        """
-        subprocess.run(self.as_python_subprocess())
-
-    def commit(self):
-        """
-        Commit and clean up after a successful execution.
-        """
-        return NotImplemented
-
     def __str__(self):
         return self.as_terminal_command()
 
     def __repr__(self):
-        return 'Command({})'.format(self.as_python_subprocess())
+        return f'Command({self.as_python_subprocess()})'
+
+
+#def setup(self):
+#    return NotImplemented
+#
+#def run(self):
+#    """
+#    Execute a command in the shell.
+#    """
+#    subprocess.run(self.as_python_subprocess())
+#
+#def commit(self):
+#    """
+#    Commit and clean up after a successful execution.
+#    """
+#    return NotImplemented
 
