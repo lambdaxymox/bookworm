2156241191850d3d9c5dadb61fec0b8d956a67ec
LambdaXymox <lambda.xymox@gmail.com>
2017-11-07 11:51:11 +0000

    Move action execution functionality into Runner class. Make Runner and Command classes abstract.

--- src/bookworm/command.py
+++ src/bookworm/command.py
@@ -1,39 +1,54 @@
-import subprocess
+import abc
 
 
-class Command:
-
-    def __init__(self, **kwargs):
-        pass
-
-    def as_arg_list(self):
-        return NotImplemented
-
+class Command(abc.ABC):
+    """
+    The ``Command`` class defines the necessary functionality for constructing
+    an executable pdf or page action. The abstract class is an interface for
+    creating and storing the necessary functionality necessary to execute the
+    action it embodies.
+    """
+    @abc.abstractmethod
     def as_terminal_command(self):
         return NotImplemented
 
+    @abc.abstractmethod
     def as_python_subprocess(self):
         return NotImplemented
 
     def __str__(self):
         return self.as_terminal_command()
 
-    def __repr__(self):
-        return f'Command({self.as_python_subprocess()})'
 
+class Runner(abc.ABC):
+    """
+    The ``Runner`` class is an abstract class that defines the required
+    methods necessary to successfully execute a ``Command``, and the handle
+    cleanup after a success or failure. The exact details are left to the
+    implementors of the abstract class. 
+    """
+    @classmethod
+    @abc.abstractmethod
+    def setup(command):
+        """
+        The ``setup`` method performs the necessary actions to setup
+        execution of a ``Command``.
+        """
+        return NotImplemented
 
-#def setup(self):
-#    return NotImplemented
-#
-#def run(self):
-#    """
-#    Execute a command in the shell.
-#    """
-#    subprocess.run(self.as_python_subprocess())
-#
-#def commit(self):
-#    """
-#    Commit and clean up after a successful execution.
-#    """
-#    return NotImplemented
+    @classmethod
+    @abc.abstractmethod
+    def execute(command):
+        """
+        The ``execute`` method executes a ``Command``.
+        """
+        return NotImplemented
+
+    @classmethod
+    @abc.abstractmethod
+    def commit(command):
+        """
+        The commit method performs cleanup after a successful execution.
+        """
+        return NotImplemented
 
