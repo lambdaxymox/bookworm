0af6d74ba62b144ca64f8ba273fa5b8de831d3d2
LambdaXymox <lambda.xymox@gmail.com>
2017-12-22 05:19:18 +0000

    Port ResamplePage tests to pytest.

--- tests/test_resample_page.py
+++ tests/test_resample_page.py
@@ -22,79 +22,81 @@ class TestResamplePage(unittest.TestCase):
         resolution = Resolution.make(resolution_val, unit_str)
         action = resample_page.make(resolution, source_file, target_file)
 
-        self.assertIsInstance(action, resample_page.ResamplePage)
+        assert isinstance(action, resample_page.ResamplePage)
 
 
-class TestResamplePageProcessArgs(unittest.TestCase):
+class TestResamplePageProcessArgs:
 
-    def setUp(self):
-        self.arg_dict = dict(
+    @pytest.fixture
+    def arg_dict(self):
+        return dict(
             input =  'sample/sample.tiff',
             output = 'sample/sample.tiff',
             units = 'PixelsPerInch'
         )
 
-    def use_resolution_val(self, resolution_val):
-        self.arg_dict['resolution'] = resolution_val
+    def use_resolution_val(self, arg_dict, resolution_val):
+        arg_dict['resolution'] = resolution_val
 
-    def test_process_args(self):
+    def test_process_args(self, arg_dict):
         """
         The argument process should create a valid ``ResamplePage``
         given valid input values.
         """
-        self.use_resolution_val(300)
-        action = resample_page.process_args(self.arg_dict)
+        self.use_resolution_val(arg_dict, 300)
+        action = resample_page.process_args(arg_dict)
 
-        self.assertIsInstance(action, resample_page.ResamplePage)
+        assert isinstance(action, resample_page.ResamplePage)
 
 
-    def test_process_args_should_reject_zero_resolution(self):
+    def test_process_args_should_reject_zero_resolution(self, arg_dict):
         """
         Given an input resolution value of zero, the argument processor
         should reject it. It is impossible to have a pdf page with a
         resolution of zero.
         """
-        self.use_resolution_val(0)
-        with self.assertRaises(ValueError):
-            resample_page.process_args(self.arg_dict)
+        self.use_resolution_val(arg_dict, 0)
+        with pytest.raises(ValueError):
+            resample_page.process_args(arg_dict)
 
 
-    def test_process_args_should_reject_negative_resolution(self):
+    def test_process_args_should_reject_negative_resolution(self, arg_dict):
         """
         Given a negative input resolution value, the argument processor
         should not accept it. Having a negative resolution value makes no
         sense.
         """
-        self.use_resolution_val(-300)
-        with self.assertRaises(ValueError):
-            resample_page.process_args(self.arg_dict)
+        self.use_resolution_val(arg_dict, -300)
+        with pytest.raises(ValueError):
+            resample_page.process_args(arg_dict)
 
 
-    def test_process_args_should_reject_noninteger_resolutions(self):
+    def test_process_args_should_reject_noninteger_resolutions(self, arg_dict):
         """
         The argument processor only supports positive integer resolutions.
         """
-        self.use_resolution_val(300.1)
-        with self.assertRaises(TypeError):
-            resample_page.process_args(self.arg_dict)
+        self.use_resolution_val(arg_dict, 300.1)
+        with pytest.raises(TypeError):
+            resample_page.process_args(arg_dict)
 
 
-class TestProcessArgsWithMissingResolutionUnits(unittest.TestCase):
+class TestProcessArgsWithMissingResolutionUnits:
 
-    def setUp(self):
-        self.arg_dict = dict(
+    @pytest.fixture
+    def arg_dict(self):
+        return dict(
             input = 'sample/sample.tiff',
             output = 'sample/sample.tiff',
             resolution = 300
         )
 
-    def test_process_args_should_reject_missing_units(self):
+    def test_process_args_should_reject_missing_units(self, arg_dict):
         """
         The argument processor should not accept an input resolution
         that has missing units.
         """
-        with self.assertRaises(KeyError):
-            resample_page.process_args(self.arg_dict)
+        with pytest.raises(KeyError):
+            resample_page.process_args(arg_dict)
 
 
 class TestRunner:
