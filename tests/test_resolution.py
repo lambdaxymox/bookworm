537ce23397d48de3533dfb9df5d281e2b504fa59
LambdaXymox <lambda.xymox@gmail.com>
2017-12-22 05:56:30 +0000

    Port resolution class test code to pytest. This completes porting the testing code.

--- tests/test_resolution.py
+++ tests/test_resolution.py
@@ -1,9 +1,9 @@
-import unittest
+import pytest
 
 from bookworm.resolution import Resolution, ResolutionUnits
 
 
-class TestResolution(unittest.TestCase):
+class TestResolution:
 
     def test_make_resolution_should_accept_correct_input(self):
         """
@@ -14,11 +14,11 @@ class TestResolution(unittest.TestCase):
         resolution_units = 'PixelsPerInch'
         resolution = Resolution.make(resolution_val, resolution_units)
 
-        self.assertIsInstance(resolution, Resolution)
-        self.assertEqual(resolution.units, ResolutionUnits.PixelsPerInch)
+        assert isinstance(resolution, Resolution)
+        assert resolution.units == ResolutionUnits.PixelsPerInch
 
 
-class TestResolutionFactoryMethod(unittest.TestCase):
+class TestResolutionFactoryMethod:
 
     def test_make_resolution_should_only_accept_certain_units(self):
         """
@@ -29,7 +29,7 @@ class TestResolutionFactoryMethod(unittest.TestCase):
         resolution_val = 600
         resolution_units = 'Potato'
 
-        self.assertRaises(ValueError, Resolution.make, resolution_val, resolution_units)
+        assert pytest.raises(ValueError, Resolution.make, resolution_val, resolution_units)
 
 
     def test_make_resolution_should_reject_negative_values(self):
@@ -40,7 +40,7 @@ class TestResolutionFactoryMethod(unittest.TestCase):
         resolution_val = -600
         resolution_units = 'PixelsPerInch'
 
-        self.assertRaises(ValueError, Resolution.make, resolution_val, resolution_units)
+        assert pytest.raises(ValueError, Resolution.make, resolution_val, resolution_units)
 
 
     def test_make_resolution_should_reject_non_integer_values(self):
@@ -50,5 +50,5 @@ class TestResolutionFactoryMethod(unittest.TestCase):
         resolution_val = 600.1
         resolution_units = 'PixelsPerInch'
 
-        self.assertRaises(TypeError, Resolution.make, resolution_val, resolution_units)
+        assert pytest.raises(TypeError, Resolution.make, resolution_val, resolution_units)
 
