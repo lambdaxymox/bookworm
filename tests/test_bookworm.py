4970183b36e48df996b1f9d30716da81edcd9bfd
LambdaXymox <lambda.xymox@gmail.com>
2018-01-21 09:34:53 +0000

    Write functional test for running bookworm with no arguments.

--- tests/test_bookworm.py
+++ tests/test_bookworm.py
@@ -1,3 +1,16 @@
 import sys
 import pytest
+import bookworm_main.bookworm_main as bookworm_main
 
+
+def test_bookworm_with_no_arguments():
+    """
+    With no arguments, the bookworm frontend should exit with an error code of
+    2. This is standard practice with unix command line utilities that pass
+    invalid arguments.
+    """
+    try:
+        bookworm_main.main(['bookworm'])
+    except SystemExit as e:
+        assert e.code == 2
+        
